022214e36a31de0b5dd86f647fd88fd1623a3ec3
pranav patel <patelpranav1111@gmail.com>
2018-12-01 06:50:04 +1100

    variable and data types of kotlin

---
+++ src/variable_datatypes.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+
+    var myDeclaredVariable: String
+
+    var myName = "prince"
+    val myAge = 25
+
+    myName = "pranav"
+    println("My name is $myName and i am $myAge years old")
+
+
+    // data types of kotlin
+    val myInt: Int = 123
+    val myDouble: Double = 123.0
+    val myFloat: Float = 12.0F
+    val myLong: Long = 12L
+    val myShort: Short = 1
+    val myByte: Byte = 1
+
+    val myString: String = "String"
+    val myBoolean: Boolean = true
+
+    val myArray: Array<String> = arrayOf("A", "B", "C", "D", "E")
+    var firstString = myArray[0]
+    println("first string in array is $firstString")
+
+    // there is char data types available in kotlin
+
+    val myNullableVariable: Int? = null
+    println("Nullable variable is $myNullableVariable")
+}
